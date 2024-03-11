e886ce0b7d34a2a84fa2f78c3d051713b6119e15
shangwei <shangwei@midu.com>
2024-03-11 23:38:47 +0800

    feat：用户参与抽奖活动库表设计

---
+++ big-market-infrastructure/src/main/java/org/upup/infrastructure/persistent/po/RaffleActivityOrder.java
@@ -0,0 +1,64 @@
+package org.upup.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author upup
+ * @description 抽奖活动单 持久化对象
+ * @date 2024/3/11 20:28
+ */
+@Data
+public class RaffleActivityOrder {
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动名称
+     */
+    private String activityName;
+
+    /**
+     * 抽奖策略ID
+     */
+    private Long strategyId;
+
+    /**
+     * 订单ID
+     */
+    private String orderId;
+
+    /**
+     * 下单时间
+     */
+    private Date orderTime;
+
+    /**
+     * 订单状态（not_used、used、expire）
+     */
+    private String state;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
